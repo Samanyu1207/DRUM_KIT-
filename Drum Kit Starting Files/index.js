cf37a9a8d242c9b4d545a065cf8668d3215f9f89
Samanyu <samanyusharma444@gmail.com>
2025-08-05 15:32:52 +0530

    animation added

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,8 +3,21 @@ var numberofdrumbuttons=document.querySelectorAll(".drum").length;
 for(var i=0; i<numberofdrumbuttons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         var buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
 
-        switch(buttonInnerHtml){
+        
+    });
+}
+
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key){
+    switch(key){
             case "w":
                 var tom1=new Audio('sounds/tom-1.mp3');
                 tom1.play();
@@ -37,10 +50,19 @@ for(var i=0; i<numberofdrumbuttons; i++){
             default:
                 console.log(buttonInnerHtml);
         }
-    });
+
 }
 
+function buttonAnimation(currentKey){
+    var activeButton=document.querySelector("."+currentKey);
 
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+
+}
 
 
 
